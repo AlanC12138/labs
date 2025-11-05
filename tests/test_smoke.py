71aed93af26afc4a957faa3973c6a38792d40de2
Yinglun Chen <alanchen0705@gmail.com>
2025-11-05 15:35:34 -0800

    Update model name for smoke tests

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -4,7 +4,7 @@ import importlib
 from fastapi.testclient import TestClient
 
 # use a tiny model to avoid large downloads in CI/local tests
-os.environ.setdefault("MODEL_NAME", "sshleifer/tiny-bart")  # ~30MB
+os.environ.setdefault("MODEL_NAME", "sshleifer/tiny-distilbart-cnn-6-6")
 os.environ.setdefault("DEVICE", "cpu")
 os.environ.setdefault("MAX_INPUT_TOKENS", "512")
 
